dfdab973355db2fe6a4c3f07ce2effbeebe30ad0
J.B.C <juanbicortelezzi@gmail.com>
2025-08-15 22:20:59 +1200

    feat: day 3 part 2

--- solutions/day_3/solution.go
+++ solutions/day_3/solution.go
@@ -7,34 +7,63 @@ import (
 	"strings"
 )
 
-func PartOne(input string) int {
-	regex := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+func parseMul(input string) (int, int) {
 
-	matches := regex.FindAllString(input, -1)
+	a, b, found := strings.Cut(input, ",")
+	if !found {
+		log.Fatalf("could not find \",\" in input: \"%s\"\n", input)
+	}
+
+	first, err := strconv.Atoi(a[4:])
+	if err != nil {
+		log.Fatalf("first item of input '%s' is not an int: '%s'", input, a[4:])
+	}
+
+	second, err := strconv.Atoi(b[:len(b)-1])
+	if err != nil {
+		log.Fatalf("second item of input '%s' is not an int: '%s'", input, b[:len(b)-1])
+	}
+
+	return first, second
+}
+
+func PartOne(input string) int {
+	matches := regexp.
+		MustCompile(`mul\(\d{1,3},\d{1,3}\)`).
+		FindAllString(input, -1)
 
 	result := 0
 	for _, match := range matches {
-		a, b, found := strings.Cut(match, ",")
-		if !found {
-			log.Fatalf("could not find \",\" in match: \"%s\"\n", match)
-		}
-
-		first, err := strconv.Atoi(a[4:])
-		if err != nil {
-			log.Fatalf("first item of match '%s' is not an int: '%s'", match, a[4:])
-		}
-
-		second, err := strconv.Atoi(b[:len(b)-1])
-		if err != nil {
-			log.Fatalf("second item of match '%s' is not an int: '%s'", match, b[:len(b)-1])
-		}
-
-		result += first * second
+		a, b := parseMul(match)
+		result += a * b
 	}
 
 	return result
 }
 
 func PartTwo(input string) int {
-	return 0
+	matches := regexp.
+		MustCompile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`).
+		FindAllString(input, -1)
+
+	result := 0
+	enabled := true
+	for _, match := range matches {
+		if match == "do()" {
+			enabled = true
+			continue
+		}
+
+		if match == "don't()" {
+			enabled = false
+			continue
+		}
+
+		if enabled {
+			a, b := parseMul(match)
+			result += a * b
+		}
+	}
+
+	return result
 }
