4442cf33d37904fafa69c99b4ab67ad6d5b2955f
J.B.C <juanbicortelezzi@gmail.com>
2025-08-29 15:28:58 +1200

    feat: day 9 part 2

--- solutions/day_9/solution.go
+++ solutions/day_9/solution.go
@@ -6,7 +6,12 @@ import (
 	"strings"
 )
 
-func parseInput(input string) []int {
+type disk struct {
+	numberOfFiles int
+	fileSystem    []int
+}
+
+func parseInput(input string) disk {
 	hugeAssArray := make([]int, 0, 100_000)
 	isFile := true
 	fileCounter := 0
@@ -32,10 +37,13 @@ func parseInput(input string) []int {
 		isFile = !isFile
 	}
 
-	return hugeAssArray
+	return disk{
+		numberOfFiles: fileCounter,
+		fileSystem:    hugeAssArray,
+	}
 }
 
-func printProblem(line []int) string {
+func problemToString(line []int, head, tail int) string {
 	str := ""
 	for _, item := range line {
 		if item == -1 {
@@ -44,33 +52,49 @@ func printProblem(line []int) string {
 			str += strconv.Itoa(item)
 		}
 	}
+
+	str += "\n"
+	for index := range line {
+		if index == head {
+			str += "h"
+			continue
+		}
+
+		if index == tail {
+			str += "t"
+			continue
+		}
+
+		str += " "
+	}
+
 	return str
 }
 
 func PartOne(input string) int {
-	arr := parseInput(input)
+	disk := parseInput(input)
 
 	head := 0
-	tail := len(arr) - 1
+	tail := len(disk.fileSystem) - 1
 
 	for head < tail {
-		if arr[head] != -1 {
+		if disk.fileSystem[head] != -1 {
 			head += 1
 			continue
 		}
 
-		if arr[tail] == -1 {
+		if disk.fileSystem[tail] == -1 {
 			tail -= 1
 			continue
 		}
 
-		arr[head], arr[tail] = arr[tail], arr[head]
+		disk.fileSystem[head], disk.fileSystem[tail] = disk.fileSystem[tail], disk.fileSystem[head]
 		head += 1
 		tail -= 1
 	}
 
 	result := 0
-	for index, num := range arr {
+	for index, num := range disk.fileSystem {
 		if num == -1 {
 			break
 		}
@@ -79,6 +103,69 @@ func PartOne(input string) int {
 	return result
 }
 
+func indexOfGapWithSize(fileSystem []int, size int) int {
+	start := -1
+	for index, item := range fileSystem {
+		isSpace := item == -1
+
+		if !isSpace {
+			start = -1
+			continue
+		}
+
+		startIsEmpty := start == -1
+		if startIsEmpty {
+			start = index
+		}
+
+		if (index - start) == size-1 {
+			return start
+		}
+	}
+	return -1
+}
+
 func PartTwo(input string) int {
-	return 0
+	disk := parseInput(input)
+
+	seenFileIds := make(map[int]struct{})
+
+	tail := len(disk.fileSystem) - 1
+
+	for len(seenFileIds) != disk.numberOfFiles {
+		fileId := disk.fileSystem[tail]
+		if fileId == -1 {
+			tail -= 1
+			continue
+		}
+
+		seenFileIds[fileId] = struct{}{}
+
+		fileLen := 0
+		for tail-fileLen >= 0 && disk.fileSystem[tail-fileLen] == fileId {
+			fileLen += 1
+		}
+
+		gapStartIndex := indexOfGapWithSize(disk.fileSystem[:tail], fileLen)
+		if gapStartIndex == -1 {
+			tail -= fileLen
+			continue
+		}
+
+		for delta := range fileLen {
+			disk.fileSystem[gapStartIndex+delta], disk.fileSystem[tail-delta] = disk.fileSystem[tail-delta], disk.fileSystem[gapStartIndex+delta]
+		}
+
+		tail -= fileLen
+
+	}
+
+	result := 0
+	for index, num := range disk.fileSystem {
+		if num == -1 {
+			continue
+		}
+		result += index * num
+	}
+	return result
 }
